d63497fb9f38a0db309a5334ca25d632e8bc370b
Pawar <pawar1550@gmail.com>
2026-01-22 18:08:42 +0530

    Add files via upload

--- T-test.R
+++ T-test.R
@@ -10,6 +10,26 @@ plot(pts,dt(pts,df=9),col='red',type='l')
 lines(density(x), col='green')
 lines(density(y), col='blue')
 
+# Apply the function for t test
+ttest = t.test(x,y)
+
+
+	Welch Two Sample t-test
+
+data:  x and y
+t = 0.80455, df = 16.71, p-value = 0.4324
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -0.5965184  1.3303208
+sample estimates:
+  mean of x   mean of y 
+ 0.03940155 -0.32749967 
+
+
+
+
+
+
 
 
 
